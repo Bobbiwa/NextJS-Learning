9d94b211d4d377617008679f48057a431bc94b18
Damon <2696774557@qq.com>
2023-08-13 20:56:19 +0800

    ssr

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 npm run dev  开发模式
-npm run start  生产模式
+npm run build 构建
+npm run start  生产模式【会启动一个以next为核心的web服务】
+
+
+
+# SSG  服务端构建
+
+概念：构建的时候生成静态页面
+
+适用：页面信息变化不频繁的，如：新闻，博客。。
+
+# SSR
+
+概念：用户请求的时候生成静态页面 
+
+适用：页面信息变化频繁的
+
+实现：服务端渲染通过getServerSIdeProps函数完成
+
+过程：当用户发起请求时，next执行getServerSIdeProps，拿到数据props传给Page渲染函数进行生成静态页面，最终返回给用户一个【崭新的页面】
+
+
+
+附加：getServerSIdeProps还提供了一个context，
+
+```javascript
+const {req,res,params,query,rosolvedUrl...} = context   //有点像express框架那个参数
+res.setHeader...还是可以响应头
+```
+
+
+
+# SSG和SSR区别
+
+- 生成静态页面的时机不同，一个是在build构建时，一个是在用户请求时
+- 易混淆点：把SSG的getStaticProps中设置fallback：true/blocking，虽然有点像SSR，但它不是，因为SSG在构建静态页面之后，页面就不会再变化了(想象页面时上的那个时间戳)下次再发起相同的请求  next会返回老的页面，如果想让页面有变化，可以在getStaticProps函数reture对象里添加revalidate：number【任意时间】,它可以设置响应头Cache-control:s-maxage=【任意时间】
+
+
+
+
+
--- pages/lists/[listId]/index.tsx
+++ pages/lists/[listId]/index.tsx
@@ -2,8 +2,12 @@ import { useRouter } from 'next/router';
 
 export default function Page({ data }: any) {
   const router = useRouter();
-  console.log(data);
-  // console.log(router.query.listId);
+  //fallback:true   页面的“第二选择”，适用于构建页面时间比较长，不让用户觉得干等着
+  // if(router.isFallback) {
+  //   return (
+  //     <h1>Loading...</h1>
+  //   )
+  // }
   return <div>{data.description}</div>;
 }
 
@@ -18,23 +22,37 @@ export default function Page({ data }: any) {
 // }
 
 /*二、全量生成(就是通过productsAll接口，map出所有paths id),来预生成所有html*/
-export async function getStaticPaths() {
-  const res = await fetch(`https://dummyjson.com/products`);
-  const data = await res.json();
-  return {
-    paths: data.products.map((c: any) => ({
-      params: { listId: c.id.toString() },
-    })),
-    fallback: false,
-  };
-}
+// export async function getStaticPaths() {
+//   const res = await fetch(`https://dummyjson.com/products`);
+//   const data = await res.json();
+//   return {
+//     paths: data.products.map((c: any) => ({
+//       params: { listId: c.id.toString() },
+//     })),
+//     fallback: false,
+//   };
+// }
 
 /*三、按需生成页面*/
 /*
 fallback
 - fallback是false，则任何未返回的路径都getStaticPaths将导致404 页面。
-- 
-*/ 
+- fallback是'blocking',则后端next为你提供服务(以next为核心的web服务)。当你请求对应的/products/[id]时，会对应构建【按需构建】,这个构建是next服务完成的，
+  next构建完成之后，会把构建的结果sever/pages/lists中，等你下次再访问相同的/products/[id]时就不需要再构建(直接走构建好的html)
+  注意：这个不是ssr，是SSG(服务端构建/生成)，它只是在你请求的时候，服务端来生成，【它生成之后就不再变了】
+- fallback是true, 你的Page(){}页面会有“第二选择”，详情看Page函数
+
+总结：什么时候按需生成：我们可以在getStaticPaths 的return paths中先默认return出用户访问频率较高的页面，其他的页面 按需生成
+*/
+
+export async function getStaticPaths() {
+  const res = await fetch(`https://dummyjson.com/products`);
+  const data = await res.json();
+  return {
+    paths: [{ params: { listId: '1' } }, { params: { listId: '2' } }],
+    fallback: true,
+  };
+}
 
 //2.接着会调用这个函数，并把getStaticPaths里paths作为参数传给这个函数
 export async function getStaticProps(context: any) {
